7fd2cc6684902ec67cdc2ad2d65434bbdaa54f39
trillsauceda <saucedais@email.arizona.edu>
2017-05-03 10:53:59 -0700

    Update inex.py
    Added Error Testing and Fixed Graph Display

--- inex.py
+++ inex.py
@@ -7,17 +7,20 @@ Class: CSC 250
 Description:
 <This program will visualize csv data>
 '''
-import pandas as pd
-from pandas import Series, DataFrame
-import matplotlib.pyplot as plt
-import sys, numpy as np
+import pandas as pd #pandas module for csv parsing
+from pandas import Series, DataFrame #objects used where data is populated
+import matplotlib.pyplot as plt #plotting module
+import sys, numpy as np #other tools
 
 plt.style.use('ggplot')
 
 print('INEX: Set data source:')
 p_file = sys.stdin.readline().strip()
 
-a_df = pd.read_csv(p_file, encoding = "ISO-8859-1")
+try:
+    a_df = pd.read_csv(p_file, encoding = "ISO-8859-1")
+except IOError:
+    quit('INEX: CSV File Not Found')
 
 print('INEX: What column(s) would you like to summarize?')
 cols = sys.stdin.readline().strip()
@@ -26,7 +29,7 @@ print('INEX: What type of plot should be generated? (text or graphical)')
 graph = sys.stdin.readline().strip()
 
 
-fig,ax = plt.subplots(figsize=(8,6))
+
 cols = cols.split('|')
 
 if len(cols) == 1:
@@ -41,7 +44,11 @@ if len(cols) == 1:
             print('|' + '{:^8}'.format(c_label) + ' |' + ' ' * 32 + ' |')
         print('|' + '-' * 43 + '|')
         #Data
-        b_series = a_df[cols[0].strip()].value_counts().sort_index()
+        try:
+            b_series = a_df[cols[0].strip()].value_counts().sort_index()
+        except KeyError:
+            quit('INDEX: Column(s) do not exist!')
+            
         MAXVAL = 0
         for col1, col2 in b_series.iteritems():
             if MAXVAL < col2.item():
@@ -50,14 +57,24 @@ if len(cols) == 1:
             reps = ((col2.item() / MAXVAL) * 32)
             print('|' + '{:>8}'.format(col1) + ' |' + '#' * int(reps.item()) + ' ' * (32 - int(reps.item())) + ' |')
         print('-' * 45)
-        
+    
+    #Graphical: 2 Columns    
     elif graph.lower() == 'graphical':
-        a_df[cols[0].strip()].value_counts().sort_index().plot(ax=ax, kind='bar')
+        fig,ax = plt.subplots(figsize=(8,6))
+        #Data
+        try:
+            a_df[cols[0].strip()].value_counts().sort_index().plot(ax=ax, kind='bar')
+        except KeyError:
+            quit('INDEX: Column(s) do not exist!')
+        
         plt.xlabel(cols[0])
         plt.ylabel('counts')
         plt.subplots_adjust(bottom=0.25)
         plt.setp(ax.get_xticklabels(),rotation='horizontal')
         plt.show()
+        
+    else:
+        print('INDEX: Plot Not Recognized')
         
 else:
     x, y = cols
@@ -73,7 +90,11 @@ else:
             print('|' + '{:^8}'.format(x) + ' |' + '{:^32}'.format(y) + ' |')
         print('|' + '-' * 43 + '|')
         #Data
-        b_series = a_df[[x.strip(), y.strip()]].groupby(x.strip()).agg(['sum','count'])
+        try:
+            b_series = a_df[[x.strip(), y.strip()]].groupby(x.strip()).agg(['sum','count'])
+        except KeyError:
+            quit('INDEX: Column(s) do not exist!')
+            
         c_series = b_series[y.strip()]['sum'] / b_series[y.strip()]['count']
         MAXVAL = 0
         for col1, col2 in c_series.iteritems():
@@ -84,12 +105,22 @@ else:
             print('|' + '{:>8}'.format(col1) + ' |' + '#' * int(reps.item()) + ' ' * (32 - int(reps.item())) + ' |')
         print('-' * 45)
     
+    #Graphical: 2 Columns
     elif graph.lower() == 'graphical':
-        t_df = a_df[[x.strip(), y.strip()]].groupby(x.strip()).agg(['sum','count'])
-        m_df = t_df[y.strip()]['sum'] / t_df[y.strip()]['count']
+        #Data
+        try:
+            t_df = a_df[[x.strip(), y.strip()]].groupby(x.strip()).agg(['sum','count'])
+        except KeyError:
+            quit('INDEX: Column(s) do not exist!')
+        
+        fig,ax = plt.subplots(figsize=(8,6))
+        m_df = t_df[y.strip()]['sum'] / t_df[y.strip()]['count'] #means
         m_df.plot(ax=ax, kind='bar')
         plt.xlabel(x.strip())
         plt.ylabel(y.strip())
         plt.subplots_adjust(bottom=0.25)
         plt.setp(ax.get_xticklabels(),rotation='horizontal')
         plt.show()
+        
+    else:
+        print('INDEX: Plot Not Recognized')
